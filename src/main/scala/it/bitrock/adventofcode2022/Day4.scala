5c50ffde4a9f4ed969152374febfa9afc8500499
Danilo Ventura <danilo.ventura@proactivity.it>
2022-12-15 21:55:46 +0100

    Refactorin Day4

--- src/main/scala/it/bitrock/adventofcode2022/Day4.scala
+++ src/main/scala/it/bitrock/adventofcode2022/Day4.scala
@@ -4,23 +4,35 @@ import scala.io.Source
 
 class Day4 {
   def part1: Int = {
+    doWork(isOverlappingPart1)
+  }
+
+  def part2: Int = {
+    doWork(isOverlappingPart2)
+  }
+
+  private def doWork(overlapFunction: String => Boolean): Int = {
     val inputFile = Source.fromResource(Day4.file)
     val input = inputFile.mkString
     input.trim
       .split("\n")
       .toList
-      .map(sectionAssignment => isOverlapping(sectionAssignment))
+      .map(sectionAssignment => overlapFunction(sectionAssignment))
       .filter(el => el == true)
       .size
   }
 
-  def isOverlapping(sectionAssignment: String): Boolean = {
+  def isOverlappingPart1(sectionAssignment: String): Boolean = {
     val sectionProvider = new SectionProvider(sectionAssignment)
     sectionProvider.isThereSectionOverlap
   }
 
-  class SectionProvider(sectionAssignment: String) {
+  def isOverlappingPart2(sectionAssignment: String): Boolean = {
+    val sectionProvider = new SectionProvider(sectionAssignment)
+    !sectionProvider.noIntersection
+  }
 
+  private class SectionProvider(sectionAssignment: String) {
     class Section(sectionExtremes: Array[String]) {
       def start: Int = sectionExtremes(0).toInt
       def end: Int = sectionExtremes(1).toInt
@@ -29,7 +41,6 @@ class Day4 {
         s"start: $start - end: $end"
       }
     }
-
     object Section {
       def apply(value: String): Section = {
         val pairSection = value.split("-")
@@ -37,8 +48,7 @@ class Day4 {
       }
     }
 
-    val pairSection = sectionAssignment.split(",")
-
+    private val pairSection = sectionAssignment.split(",")
     val firstSection = Section(pairSection(0))
     val secondSection = Section(pairSection(1))
 
@@ -49,6 +59,14 @@ class Day4 {
         (firstSection.start >= secondSection.start && firstSection.end <= secondSection.end)
       )
     }
+
+    def noIntersection: Boolean = {
+      (
+        (secondSection.start > firstSection.end && secondSection.end > firstSection.end)
+        ||
+        (firstSection.start > secondSection.end && firstSection.end > secondSection.end)
+      )
+    }
   }
 
 }
@@ -57,15 +75,22 @@ object Day4 extends App {
   private val file = "input-day4.txt"
   val day4 = new Day4
   println(s"Answer day4-part1: ${day4.part1}")
+  println(s"Answer day4-part2: ${day4.part2}")
   /*
-  println(day4.isOverlapping("2-4,6-8"))
-  println(day4.isOverlapping("2-3,4-5"))
-  println(day4.isOverlapping("5-6,7-9"))
-  println(day4.isOverlapping("2-8,3-7"))
-  println(day4.isOverlapping("6-6,4-6"))
-  println(day4.isOverlapping("2-6,4-8"))
-  println(day4.isOverlapping("36-78,21-65"))
+  println(day4.isOverlappingPart1("2-4,6-8"))
+  println(day4.isOverlappingPart1("2-3,4-5"))
+  println(day4.isOverlappingPart1("5-6,7-9"))
+  println(day4.isOverlappingPart1("2-8,3-7"))
+  println(day4.isOverlappingPart1("6-6,4-6"))
+  println(day4.isOverlappingPart1("2-6,4-8"))
+  println(day4.isOverlappingPart1("36-78,21-65"))
 
+  println(day4.isOverlappingPart2("2-4,6-8"))
+  println(day4.isOverlappingPart2("2-3,4-5"))
+  println(day4.isOverlappingPart2("5-7,7-9"))
+  println(day4.isOverlappingPart2("2-8,3-7"))
+  println(day4.isOverlappingPart2("6-6,4-6"))
+  println(day4.isOverlappingPart2("2-6,4-8"))
+  println(day4.isOverlappingPart2("36-78,21-65"))
    */
-
 }
